647ae3a77dc4543575c377db23daaa966133122e
Alamgir Jhanzaib <alamgirjhanzaib786@gmail.com>
2021-06-16 17:09:38 +0500

    initial uploads

---
+++ app/src/main/java/org/cubiit/admanager/MainActivity.java
@@ -0,0 +1,14 @@
+package org.cubiit.admanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
